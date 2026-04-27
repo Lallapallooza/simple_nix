d90a2aaf35cc65626ec588d56730fd635339d63b
vitalii <vitaly.shutov1@gmail.com>
2026-04-27 11:22:11 +0100

    fix: suspend

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -76,12 +76,12 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
-  services.logind.extraConfig = ''
-    HandleSuspendKey=ignore
-    HandleHibernateKey=ignore
-    HandleLidSwitch=ignore
-    HandleLidSwitchExternalPower=ignore
-    HandleLidSwitchDocked=ignore
-    IdleAction=ignore
-  '';
+  services.logind.settings.Login = {
+    HandleSuspendKey = "ignore";
+    HandleHibernateKey = "ignore";
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+    IdleAction = "ignore";
+  };
 }
